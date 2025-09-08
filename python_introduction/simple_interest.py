04dbe1f1796742653e4ad95059cfc9dfb58b5f33
Maile-ai <maile.s.mokete@gmail.com>
2025-09-08 15:40:38 +0200

    Made changes of variable interest to simple_interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,5 +3,5 @@ principal = 1000
 rate = 0.05    
 time = 3    # time in years
 # calculate simple interest
-interest = (principal * rate * time)
-print("The simple interest is:", interest)
+simple_interest = (principal * rate * time)
+print("The simple interest is:", simple_interest)
