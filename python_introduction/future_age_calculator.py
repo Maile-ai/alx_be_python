d41b024101f2532c6edcb9bea58b776cecf1f2b8
Maile-ai <maile.s.mokete@gmail.com>
2025-09-08 16:09:14 +0200

    Calculate future age geven the current age from the console input

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+# Calculating age in the future
+age = int(input("Enter your current age: "))
+future_year = 2050
+current_year = 2023
+years_until_future = future_year - current_year
+future_age = age + years_until_future  
+print("In year", future_year, "you will be", future_age, "years old.")
