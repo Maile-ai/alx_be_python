d6ff5d3ef6c177cf98eff4df0715a08d08955eca
Maile-ai <maile.s.mokete@gmail.com>
2025-09-08 15:31:14 +0200

    Calculated the interest and printed the result

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+# basic python operations
+principal = 1000
+rate = 0.05    
+time = 3    # time in years
+# calculate simple interest
+interest = (principal * rate * time)
+print("The simple interest is:", interest)
