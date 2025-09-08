251684b770e4dccafe4cbbc56b783305de53066d
Maile-ai <maile.s.mokete@gmail.com>
2025-09-08 15:46:13 +0200

    Made some changes to the code

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,6 @@
 # basic python operations
 principal = 1000
 rate = 0.05    
-time = 3    # time in years
-# calculate simple interest
-simple_interest = (principal * rate * time)
+time = 3   
+simple_interest = principal * rate * time
 print("The simple interest is:", simple_interest)
